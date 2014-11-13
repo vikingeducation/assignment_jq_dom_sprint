3d879a3764ba265b8610929e21b95f27fccea395
Michael Alexander <alexandermw@gmail.com>
2014-11-12 21:33:17 -0800

    fix appends

--- sprint.js
+++ sprint.js
@@ -1,14 +1,16 @@
 $(document).ready( function(){
 
-  $('h1').replaceWith('<h1>Miking Code School</h1>');
-  
-  $('h4').after('<ol><li>Change the h1 to something cheeky</li> \
-    <li>Inside the container for "Upcoming Traversals", create and insert an ordered list which corresponds to this one.</li> \
-    <li>Make all sad classes into happy ones.</li> \
-    <li>Make the annoying popup link point instead to http://www.cashcats.biz.</li> \
-    <li>Change the positioning of the annoying popup so it is on the right side of the screen (it is okay to use direct CSS here). Make it 30 pixels lower than its current position by utilizing its current top value.</li> \
-    <li>Replace the ellipsis ... in one of the suggested topics with content of your choice -- but do so by traversing from a different element.</li> \
-    <li>Replace the form input with a textarea instead of a input type text .</li></ol>');
+  $('h1').text('Miking Code School');
+
+
+  $('h4').after('<ol></ol>');
+  $('ol').append('<li>Change the h1 to something cheeky</li>')
+  $('ol').append('<li>Inside the container for "Upcoming Traversals", create and insert an ordered list which corresponds to this one.</li>');
+  $('ol').append('<li>Make all sad classes into happy ones.</li>')
+  $('ol').append('<li>Make the annoying popup link point instead to http://www.cashcats.biz.</li>');
+  $('ol').append('<li>Change the positioning of the annoying popup so it is on the right side of the screen (it is okay to use direct CSS here). Make it 30 pixels lower than its current position by utilizing its current top value.</li>');
+  $('ol').append('<li>Replace the ellipsis ... in one of the suggested topics with content of your choice -- but do so by traversing from a different element.</li>');
+  $('ol').append('<li>Replace the form input with a textarea instead of a input type text .</li>');
 
 
   var $sads = $('.sad');

250f60f2ebf4368e872b31ffaa186e856d550dfe
shadefinale <shadefinale@gmail.com>
2015-08-25 16:07:05 -0500

    Change ul to ol.

--- mover.js
+++ mover.js
@@ -2,14 +2,14 @@ $("h1").text("This is not a drill: CLG 3-0 TSM");
 
 $($("ul").children()[6]).text("My new text.");
 
-$($("h4")[0]).html("<h4>Upcoming Traversals:</h4><ul><li>Change the h1 to something cheeky</li>\
+$($("h4")[0]).html("<h4>Upcoming Traversals:</h4><ol><li>Change the h1 to something cheeky</li>\
   <li>Inside the container for \"Upcoming Traversals\", create and insert an ordered list which corresponds to this one.</li>\
   <li>Make all sad classes into happy ones.</li>\
   <li>Make the annoying popup link point instead to http://www.cashcats.biz.</li>\
   <li>Change the positioning of the annoying popup so it is on the right side of the screen (it's okay to use direct CSS here). Make it 30 pixels lower than its current position by utilizing its current top value.</li>\
   <li>Replace the ellipsis ... in one of the suggested topics with content of your choice -- but do so by traversing from a different element.</li>\
   <li>Replace the form input with a textarea instead of a input type=\"text\".</li>\
-  <ul>")
+  <ol>")
 
 $(".sad").removeClass("sad").addClass("happy");
 $("#annoying-popup a").attr("href", "http://www.cashcats.biz");

58caf4222fa53a216bee9751ba4fbad9d54a272f
alokp <alokpra@gmail.com>
2015-08-25 18:27:12 -0700

    complete

--- dom.js
+++ dom.js
@@ -1,39 +1,42 @@
-// Change heading
-$("h1").html("Something Cheeky");
+$(document).ready(function(){
 
-// Add list below Upcoming Traversals
-$first = $("h4");
-$newLiArr = [];
-$newLiArr.push($('<li> This is new 1! </li>'));
-$newLiArr.push($('<li> This is new 2! </li>'));
-$newLiArr.push($('<li> This is new 3! </li>'));
-$newLiArr.push($('<li> This is new 4! </li>'));
+  // Change heading
+  $("h1").html("Something Cheeky");
 
-for(var i=0; i < $newLiArr.length; i++) {
-  $newLiArr[i].insertAfter($first);
-}
+  // Add list below Upcoming Traversals
+  $first = $("h4");
+  $newLiArr = [];
+  $newLiArr.push($('<li> This is new 1! </li>'));
+  $newLiArr.push($('<li> This is new 2! </li>'));
+  $newLiArr.push($('<li> This is new 3! </li>'));
+  $newLiArr.push($('<li> This is new 4! </li>'));
 
-// Convert class of items from sad to happy
-var $sadItems = $('.sad');
-$sadItems.removeClass('sad');
-$sadItems.addClass('happy');
+  for(var i=0; i < $newLiArr.length; i++) {
+    $newLiArr[i].insertAfter($first);
+  }
 
-// Change link of flash msg
-var $link = $('#annoying-popup a');
-$link.attr('href', 'http://www.cashcats.biz');
+  // Convert class of items from sad to happy
+  var $sadItems = $('.sad');
+  $sadItems.removeClass('sad');
+  $sadItems.addClass('happy');
 
-var $popUp = $('#annoying-popup');
+  // Change link of flash msg
+  var $link = $('#annoying-popup a');
+  $link.attr('href', 'http://www.cashcats.biz');
 
-$popUp.css({'left': '80%', "top": "30px" });
+  // Change position of flash msg
+  var $popUp = $('#annoying-popup');
+  $popUp.css({'left': '80%', "top": "30px" });
 
-$div = $(".suggested-topics");
-//I dont get chainging!
-$children = $div.children();
-$targetList = $children.first().next();
-$target = $targetList.children().eq(6);
-$target.html("CHANGED");
+  // Change 7th element in list
+  $div = $(".suggested-topics");
+  $targetList = $div.children().first().next();
+  $target = $targetList.children().eq(6);
+  $target.html("CHANGED!");
 
-$form = $('form');
-$targetTextArea = $form.children().eq(0);
-$targetTextArea.replaceWith('<textarea>');
+  // Change form input element
+  $form = $('form');
+  $targetTextArea = $form.children().eq(0);
+  $targetTextArea.replaceWith('<textarea>');
 
+});

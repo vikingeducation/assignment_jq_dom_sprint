dc4a43d463c4ac1715b7ce3cc7d0d284b9a4cf40
Justin Mullis <justin.mullis@gmail.com>
2016-01-26 18:48:34 -0500

    erik’s notes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var listItems = ["Make all sad classes into happy ones.",
   "Make the annoying popup link point instead to www.cashcats.biz.",
   "Change the positioning of the annoying popup so it is on the right side \
@@ -6,29 +8,29 @@ var listItems = ["Make all sad classes into happy ones.",
   "Replace the ellipsis ... in one of the suggested topics with content \
   of your choice -- but do so by traversing from a different element.",
   "Replace the form input with a \<textarea\> instead of a \
-  <input type='text'>."]
+  <input type='text'>."];
 
 $( document ).ready(function(){
 
-  $("h1").text("Won't someone stop this blinking?!?")
+  $("h1").text("Won't someone stop this blinking?!?");
 
   //Had to stop it!
-  $("#annoying-popup").removeClass("blink")
+  $("#annoying-popup").removeClass("blink");
 
-  var list = $(".info-box h4").after("<ul/>")
+  var list = $(".info-box h4").after("<ul/>");
   $.each(listItems, function(i,val) {
-    listItem = document.createElement("li");
+    var listItem = document.createElement("li");
     $(listItem).text(val);
     list.append(listItem);
   });
 
-  $(".sad").removeClass("sad").addClass("happy")
+  $(".sad").removeClass("sad").addClass("happy");
 
-  $("#annoying-popup a").attr("href", "http://www.cashcats.biz")
+  $("#annoying-popup a").attr("href", "http://www.cashcats.biz");
 
-  $("#annoying-popup").css({"right": 0, "top": "+=30"})
+  $("#annoying-popup").css({"right": 0, "top": "+=30"});
 
-  $(".suggested-topics li:last-child").prev().text("Um?")
+  $(".suggested-topics li:last-child").prev().text("Um?");
 
-  $(".input-form :text").replaceWith("<textarea placeholder='sup'>")
-})
+  $(".input-form :text").replaceWith("<textarea placeholder='sup'>");
+});

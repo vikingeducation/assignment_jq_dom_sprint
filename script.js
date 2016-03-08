c4bcd8b275ddd830707d6818b0e7e56055547917
Andrew Baik <and.baik@gmail.com>
2016-03-08 13:56:16 -0500

    some semicolon added

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   }, 500, 'swing', function() {
     $('h1').animate({
       fontSize: '30px'
-    })
+    });
   });
 
   topics = ['Change the <h1> to something cheeky',
@@ -13,10 +13,10 @@ $(document).ready(function() {
   'Make the annoying popup link point instead to http://www.cashcats.biz.',
   'Change the positioning of the annoying popup so it is on the right side of the screen (it\'s okay to use direct CSS here). Make it 30 pixels lower than its current position by utilizing its current top value.',
   'Replace the ellipsis ... in one of the suggested topics with content of your choice -- but do so by traversing from a different element.',
-  'Replace the form input with a <textarea> instead of a <input type="text">.']
-  orderedList = $("<ol></ol>")
+  'Replace the form input with a <textarea> instead of a <input type="text">.'];
+  orderedList = $("<ol></ol>");
   for (var i = 0; i < topics.length; ++i) {
-    orderedList.append("<li>" + topics[i].replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;') + "</li>")
+    orderedList.append("<li>" + topics[i].replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;') + "</li>");
   }
-  $('.info-box').append(orderedList)
+  $('.info-box').append(orderedList);
 });

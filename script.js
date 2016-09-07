2ff7b8f8185be45b05a76a2739a503d8e7708491
Mike Lee <asackofwheat@gmail.com>
2016-09-07 18:02:48 -0400

    popuplink

---
+++ script.js
@@ -0,0 +1,23 @@
+var h1 = $("h1");
+h1.html("Whats up");
+
+var list = $("<ul>")
+  .append("<li>1. Change the h1 to something cheeky</li>")
+  .append("<li>2. Inside the container for Upcoming Traversals, create and insert an ordered list which corresponds to this one.</li>")
+  .append("<li>3. Make all sad classes into happy ones.</li>")
+  .append("<li>4. Make the annoying popup link point instead to http://www.cashcats.biz.</li>")
+  .append("<li>5. Change the positioning of the annoying popup so it is on the right side of the screen (it's okay to use direct CSS here). Make it 30 pixels lower than its current position by utilizing its current top value.</li>")
+  .append("<li>6. Replace the ellipsis ... in one of the suggested topics with content of your choice -- but do so by traversing from a different element.</li>")
+  .append("<li>7. Replace the form input with a textarea instead of a input type=\"text\"</li>");
+
+var infoDiv = $(".info-box");
+var traversals = $("h4");
+traversals.after(list);
+
+var sadDivs = $(".sad");
+sadDivs.toggleClass("sad").toggleClass("happy");
+
+var popupLink = $("#annoying-popup a");
+popupLink.attr("href", "http://www.cashcats.biz");
+
+popupLink.parent.css("height", "400px");

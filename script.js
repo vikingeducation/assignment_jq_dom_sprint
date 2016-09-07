86438f77efd94ea8516c1965612748a6259667ca
David Jiang <davidjgrey@gmail>
2016-09-07 15:19:21 -0700

    changed sads to happy

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 $(document).ready(
   // #1
   // change h1
@@ -6,11 +5,22 @@ $(document).ready(
     $("h1").html("DAVID DOESN'T REALLY LIKE POKEMON.");
   //# 2 put ordered list into upcoming traversals
   //grab the list from suggest topics
-  // change the list into ordered
-  // append it to upcoming traversal
-    var $list = $('.suggested-topics ul li');
+  //clone the list
+  //create an empty ol element
+  // add each li element into the empty ol
+  // append the ol to upcoming traversal
+    var $list = $('.suggested-topics ul li').clone();
+    var $newList = $('<ol></ol>');
+    $list.each(function(){
+      $newList.append(this);
+    })
+    $('.info-box').append($newList);
 
-  };
+  //# 3 make all sad classes into happy ones
+    var $sads = $('.sad');
+    $sads.addClass('happy');
+    $sads.removeClass('sad');
+  }
 );
 
 

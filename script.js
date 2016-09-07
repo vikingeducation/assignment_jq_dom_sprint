20681c083dc45799b12adfb5d2695e5fb875021d
Adrian Mui <adrian.mui@gmail.com>
2016-09-07 17:57:28 -0500

    fin

--- script.js
+++ script.js
@@ -25,9 +25,17 @@ $(document).ready(
   //# 4 change annoying link to another annoying link thnx kit
     $(".blink a").attr("href", "http://www.cashcats.biz");
 
-  //# 5
-    $(".blink").css("top", "40px")
+  //# 5 make annoying link 30 pixels lower and to the right.
+    $(".blink").css("top", "40px").css("right", "0px");
+
+  //# 6 Replace the ellipsis ... by traversing from different element
+    $(".info-box ol li:nth-child(7)").html("adrian is pretty damn cool COOOL COOL");
+
+  //#7 Replace form input with textArea
+    $(".input-form input").first().replaceWith($("<textarea></textarea>"))
   }
+
+  
 
   
   

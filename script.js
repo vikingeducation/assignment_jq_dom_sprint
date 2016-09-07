9005364524cd57bf960dbd6f31310e2d08ba2006
Adrian Mui <adrian.mui@gmail.com>
2016-09-07 16:37:03 -0500

    jquery practice

---
+++ script.js
@@ -0,0 +1,27 @@
+
+$(document).ready(
+  
+
+  // #1
+  // change h1
+  function() { 
+    $("h1").html("DAVID DOESN'T REALLY LIKE POKEMON.");
+  };
+
+  //# 2 put ordered list into upcoming traversals
+  //grab the list from suggest topics
+  // change the list into ordered
+  // append it to upcoming traversals
+  function() {
+
+  var $list = 
+
+  }
+
+
+
+);
+
+
+
+

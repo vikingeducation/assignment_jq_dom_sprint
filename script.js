bbd0ade5e488f1a567f8c87df9e09ee8752757a7
Ann Allan <ann.cristina.allan@gmail.com>
2017-01-20 17:10:23 -0500

    Add h1,ol, happy

---
+++ script.js
@@ -0,0 +1,5 @@
+$(function() {
+   $('h1').html("Tackle and Matriculate");
+   $("h4").append('<ol><li>Change h1</li><li>Insert Ordered List</li><li>Make Sad Classes Happy</li><li>Change Annoying Popup Link</li><li>Move Popup Link</li><Replace Ellipsis</li><li>Replace Form Input</li></ol>');
+   $('.sad').removeClass('sad').addClass('happy');
+});

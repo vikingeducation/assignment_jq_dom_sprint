bc1541128000221e563aad4f24fffac0aab27ae5
Renzo Tomlinson <rttomlinson@gmail.com>
2017-02-11 07:37:44 +0000

    Escaped HTML in Task 2 List

--- scripts.js
+++ scripts.js
@@ -4,13 +4,13 @@ $(document).ready(function () {
     
     /*Task 2 */
     var $traversalList = $("<ol></ol>");
-    $traversalList.append($("<li>Change the \<h1\> to something cheeky</li>"));
-    $traversalList.append($("<li>Inside the container for \"Upcoming Traversals\", create and insert an ordered list which corresponds to this one.</li>"));
+    $traversalList.append($("<li>Change the &#60;h1&#62; to something cheeky</li>"));
+    $traversalList.append($("<li>Inside the container for &quot;Upcoming Traversals&quot;, create and insert an ordered list which corresponds to this one.</li>"));
     $traversalList.append($("<li>Make all sad classes into happy ones.</li>"));
     $traversalList.append($("<li>Make the annoying popup link point instead to http://www.cashcats.biz.</li>"));
     $traversalList.append($("<li>Change the positioning of the annoying popup so it is on the right side of the screen (it's okay to use direct CSS here). Make it 30 pixels lower than its current position by utilizing its current top value.</li>"));
     $traversalList.append($("<li>Replace the ellipsis ... in one of the suggested topics with content of your choice -- but do so by traversing from a different element.</li>"));
-    $traversalList.append($("<li>Replace the form input with a \<textarea\> instead of a \<input type=\"text\"\>.</li>"));
+    $traversalList.append($("<li>Replace the form input with a &#60;textarea&#62; instead of a &#60;input type=&quot;text&quot;&#62;.</li>"));
     $(".info-box").append($traversalList);
     
     
@@ -18,4 +18,7 @@ $(document).ready(function () {
     $('[class*="sad"')
         .removeClass("sad")
         .addClass("happy");
+        
+    /*Task 4*/
+    $("#annoying-popup > a")
 });

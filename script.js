ca8901879d7ce83911add896021221f0f669a72a
rohb0t <edroh90@gmail.com>
2017-03-16 21:33:34 -0700

    Finished jq dom sprint

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('h1').html("I am cheeky!");
+  $('.info-box').append("<ol><li>Change the &lt;h1&gt; to something cheeky</li><li>Inside the container for 'Upcoming Traversals', create and insert an ordered list which corresponds to this one.</li><li>Make all sad classes into happy ones.</li><li>Make the annoying popup link point instead to http://www.cashcats.biz</li><li>Change the positioning of the annoying popup so it is on the right side of the screen (it's okay to use direct CSS here). Make it 30 pixels lower than its current position by utilizing its current top value.</li><li>Replace the ellipsis ... in one of the suggested topics with content of your choice -- but do so by traversing from a different element.</li><li>Replace the form input with a &lt;textarea&gt; instead of &lt;input type='text'&gt;</li></ol>");
+  $('.sad').removeClass('sad').addClass('happy');
+  $('a').attr("href", "http://cashcats.biz");
+  $('li').last().prev().html("HELLO");
+  $('input[type=text]').replaceWith('<textarea></textarea>');
+});

85aa99ce1da67712cb21dd6c677b3f0871a6fc77
Stephen Akuffo <stephen.akuffo@gmail.com>
2017-05-08 23:26:10 -0400

    sprint done

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ $( document ).ready(function(){
   $('.info-box ol').append('<li>Make All Sad Classes into happy ones</li>')
                     .append('<li>Change the popup link</li>')
                     .append('<li>Change the popup positioning</li>')
-                    .append('<li>Replace the ellipsis</li>');
+                    .append('<li>Replace the ellipsis</li>')
+
 
   // 3: Make all sad classes into happy ones
   $('.sad')
@@ -24,9 +25,15 @@ $( document ).ready(function(){
   // 5: Change the positioning of the annoying popup so it is on the 
   // right side of the screen (it's okay to use direct CSS here). 
   // Make it 30 pixels lower than its current position by utilizing its current top value.
+  $('#annoying-popup').css('right', '10px');
+  $('#annoying-popup').css('top', '10px');
 
   // 6: Replace the ellipsis ... in one of the suggested topics with content of your choice 
   // -- but do so by traversing from a different element.;
   let topicToChange = $('.suggested-topics ul li').last().prev();
   topicToChange.html('Toggles');
+
+  // 7. Replace text input with textarea
+  let inputToChange = $('.input-form input[type="text"]')
+  inputToChange.replaceWith('<textarea placeholder="Tell me a story!"></textarea>')
 });

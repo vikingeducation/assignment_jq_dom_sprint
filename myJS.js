fc219b9251b2752ca6a8fe4eded82058784ac669
alanmodia <github@alanmillar.com>
2017-05-19 07:35:54 -0700

    Updated Item 6 Replace Ellipse to use traversal

--- myJS.js
+++ myJS.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
 
-var $changeH1 = $( "h1" );
+// is this a good pattern, or is it doing too many queries?
+var $changeH1 = $( "h1" ); // could be more specific
 	// console.log($changeH1.);
 	if ( $changeH1.length ) {
 		$changeH1.text("Cheeky Text");
 		};
 
+// should I be doing a variable and checking length on all of these? 
 $('.info-box.sad h4').append("<ul><li>Sad to Happy</li><li>CashCats</li><li>Annoying Position</li><li>Replace an ellipse</li><li>Input Area</li></ul>");
 
 // change class sad to happy
@@ -14,25 +16,26 @@ $('.info-box.sad h4').append("<ul><li>Sad to Happy</li><li>CashCats</li><li>Anno
 
 // make annoying link point to CashCats
 var $popup = $('#annoying-popup a');
+
 	$popup.prop('href', 'http://www.cashcats.biz');
 	// alert($popup.prop('href'));
-
 // stick the blinker right and down another 30px, and regain sanity by muting the flashing
 $( '#annoying-popup' )
   .css( 'right', '0px')
   .css( 'top', '40px')
   .css( 'opacity', 0.1);
 
+// I traversed from the last child of the ul to the second to last, and replace the ellipse
+// var $changeEllipse = $( 'div.suggested-topics ul li:nth-last-child(2)');
+// feedback: this is a css method, I need to use jQ to find parent and then child, or similar
+// https://api.jquery.com/category/traversing/tree-traversal/
+var $changeEllipse = $("div.suggested-topics li:contains('...')");
 
-// traverse from the last child of the ul to the second to last, and replace the ellipse
-var $changeEllipse = $( 'div.suggested-topics ul li:nth-last-child(2)');
+
 	// console.log($changeEllipse.text());
-	var newText = $changeEllipse.text("Foo Fighters");
+var newText = $changeEllipse.text("Foo Fighters");
 	// console.log(newText.text());
 
-// note to self
-// http://learn.jquery.com/using-jquery-core/faq/how-do-i-replace-text-from-the-3rd-element-of-a-list-of-10-items/
-// total waste of time
 
 // replace the form input type="text" with "textarea"
 var $swapInputs = $( 'div.input-form').find(':input:first').replaceWith('<textarea></textarea><br>');

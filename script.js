f19dd67dfaad003c33973afcbf8991ad641adc01
Will Timpson <will.timpson@gmail.com>
2017-06-28 00:09:25 -0400

    Upcoming Traversals

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 $(
   function() {
     // Update the headline
-    $('h1').text("Six pints of bitter... the world's about to end.")
+    $('h1').text("Six pints of bitter... the world's about to end.");
+
+    // List traversals
+    $('div.info-box')
+      .append( $('<ol>')
+        .append( $.map(
+          ['Mount Everest', 'The Great Wall of China', 'The Gobi Desert'],
+          function(traversal) {
+            return $('<li>').text(traversal)
+          }
+        ) )
+      )
+
   }
 );

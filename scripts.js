ed6a7cdc725ecb531e628dec514b8a2631e26bbc
anne <richardson.ae@gmail.com>
2017-07-14 17:02:11 -0500

    Solution to Q2: insert list

--- scripts.js
+++ scripts.js
@@ -3,5 +3,15 @@ $(document).ready(function(){
   // Change the <h1> to something cheeky
   $( 'header h1' ).text('something cheekier than that');
 
+  // Inside the container for 'Upcoming Traversals', create and insert an ordered list of upcoming traversals.
+  $newOl = $('<ol></ol>');
+  $( '.info-box' ).append($newOl);
+
+  items = ['Toggle classes', 'Change text', 'Change positioning', 'Replace HTML elements'];
+
+  items.forEach(function(item){
+    $( '.info-box ol' ).append($('<li>'+item+'</li>'));
+  });
+
 
 })

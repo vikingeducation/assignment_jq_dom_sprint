4f9fb14a801411909b4438d8cdcdcfb7f49616fd
anne <richardson.ae@gmail.com>
2017-07-15 13:46:32 -0500

    Expand li text to full content

--- scripts.js
+++ scripts.js
@@ -7,7 +7,15 @@ $(document).ready(function(){
   $newOl = $('<ol></ol>');
   $( '.info-box' ).append($newOl);
 
-  items = ['Toggle classes', 'Change text', 'Change positioning', 'Replace HTML elements'];
+  items = [
+    'Change the <h1> to something cheeky',
+    'Inside the container for "Upcoming Traversals", create and insert an ordered list which corresponds to this one.',
+    'Make all sad classes into happy ones.',
+    'Make the annoying popup link point instead to http://www.cashcats.biz.',
+    "Change the positioning of the annoying popup so it is on the right side of the screen (it's okay to use direct CSS here). Make it 30 pixels lower than its current position by utilizing its current top value.",
+    'Replace the ellipsis ... in one of the suggested topics with content of your choice -- but do so by traversing from a different element.',
+    'Replace the form input with a <textarea> instead of a <input type="text">.'
+  ];
 
   items.forEach(function(item){
     $( '.info-box ol' ).append($('<li>'+item+'</li>'));

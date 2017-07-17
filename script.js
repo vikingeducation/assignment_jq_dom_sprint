d3cd9d6aa753f3ad300fe7ff7ab11beee9de807a
Paul Savage <p.a.savage@btinternet.com>
2017-07-17 15:07:18 +0100

    Rest of changes

--- script.js
+++ script.js
@@ -20,4 +20,20 @@ $(".sad").addClass("happy").removeClass("sad");
 //	Make the annoying popup link point instead to http://www.cashcats.biz
 $("#annoying-popup a").prop("href", "http://www.cashcats.biz/");
 
+//	Change the positioning of the annoying popup so it is on the right side of the screen (it's okay to use direct CSS here). Make it 30 pixels lower than its current position by utilizing its current top value
+$("#annoying-popup").css("right", "10px").css("top", "+=30px");
+
+//	Replace the ellipsis ... in one of the suggested topics with the content of your choice -- but do so by traversing from a different element
+$(".suggested-topics ul li")
+	.last()
+	.prev()
+	.text("content of your choice");
+
+//	Replace the form input with a <textarea> instead of a <input type="text">
+var $input = $("form input[type='text']");
+var $textarea = $("<textarea></textarea>")
+	.prop("placeholder", $input.prop("placeholder"));
+$input.after($textarea);
+$input.remove();
+
 });

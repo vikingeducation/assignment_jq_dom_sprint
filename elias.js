24db3ed18fe77714a770b7497c95dd0cec03b83b
Elias Hantula <ehantula@indiana.edu>
2017-09-27 16:40:14 -0400

    completed project

--- elias.js
+++ elias.js
@@ -1,1 +1,12 @@
-document.getElementsByTagName("h1").innerHTML = "Lets get this changed"
+
+$(document).ready(function() {
+$("h1").html("Look at me Now!");
+$("h4").append("<ol><li>parents</li><li>children</li><li>filter</li><li>find</li></ol>");
+$(".sad").removeClass().addClass("happy");
+$(".blink a").attr("href", "http://www.cashcats.biz");
+$(".suggested-topics ul li:nth-child(7)").html("Wicked widgets")
+$(":text").replaceWith("<textarea>Tell me a story!</textarea>")
+
+
+});
+

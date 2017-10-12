def057aaec4073fb6e418aacb1f97bd37cc6015d
M. Manousos <mmanousos@gmail.com>
2017-10-11 17:14:58 -0700

    corrections to solutions

--- applications.js
+++ applications.js
@@ -17,7 +17,13 @@ $(document).ready(function(){
     // have to make sure to specify to select the link element 'a'
     
     // 5. move popup to right and 30 pixels down
-    $('#annoying-popup').css({'position': 'absolute', 'right': '0', 'top': '30px'});
+    $('#annoying-popup')
+        .css({
+            'position': 'absolute', 
+            'right': '0', 
+            'top': '+=30px'
+        //modified to be 30px from original position, not 30px from top (misread the assignment)
+    });
     
     // 6. replace elepsis 
     $('.suggested-topics')
@@ -27,8 +33,23 @@ $(document).ready(function(){
         .replaceWith("<li>Super new content!</li>");
     
     // 7. replace form input with 'textarea'
-    var textArea = $('<input type="textarea" placeholder="Tell me a story!"</input>'); 
-    $('.input-form input[type=text]').replaceWith(textArea);  
+    
+    /*var textArea = $('<input type="textarea" placeholder="Tell me a story!"</input>'); 
+    $('.input-form input[type=text]').replaceWith(textArea);  */
     // not sure why it doesn't display differently but according to the Dev Tools, the type has been adjusted. 
+    
+    
+    //this code replaces the area correctly but hard codes the placeholder instead of pulling the existing from the html. 
+    var areaReplace = $('<textarea placeholder="Tell me a story!"></textarea>');
+    $('.input-form input[type=text]').replaceWith(areaReplace);
+
+    
+    //code from other student's solution that does pull the placeholder text from the html    
+   /* var $input = $('.input-form input[type=text]');
+    var placeholder = $input.attr('placeholder');
+    $('.input-form input[type=text]').remove();
+    var $textarea = $('<textarea></textarea>')
+      .attr('placeholder', placeholder);
+    $('.input-form form').prepend($textarea);*/ 
     
 });

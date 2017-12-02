098a1d6de49b13d50a85845b6cc300c275d94b11
ksc23 <ksc.main@gmail.com>
2017-12-02 12:26:03 -0800

    Fix spacing

--- scripts.js
+++ scripts.js
@@ -6,19 +6,20 @@ $(document).ready(function(){
   let $li1 = $('<li>').html(`Make all <code>sad</code> classes into <code>happy
     </code> ones`);
   
-  let $li2 = $('<li>').html(`Make the annoying popup link point instead to <a h
-    ref="http://www.cashcats.biz">http://www.cashcats.biz</a>`);
+  let $li2 = $('<li>').html(`Make the annoying popup link point instead to <a
+    href="http://www.cashcats.biz">http://www.cashcats.biz</a>`);
 
   let $li3 = $('<li>').html(`Change the positioning of the annoying popup so it
-    is on the right side of the screen. Make it 30 pixels lower than its curren
-    t position by utilizing its current <code>top</code> value`);
+    is on the right side of the screen. Make it 30 pixels lower than its current
+    position by utilizing its current <code>top</code> value`);
 
-  let $li4 = $('<li>').html(`Replace the ellipses <code>...</code> in one of th
-    e suggested topics with the content of your choice -- but do so by traversi
-    ng from a different element.`);
+  let $li4 = $('<li>').html(`Replace the ellipses <code>...</code> in one of the
+    suggested topics with the content of your choice -- but do so by traversing
+    from a different element.`);
 
-  let $li5 = $('<li>').html(`Replace the form input with a <code>&lt;textarea&g
-    t;</code> instead of a <code>&lt;input type="text"&gt;</code>`);
+  let $li5 = $('<li>').html(`Replace the form input with a
+    <code>&lt;textarea&gt;</code> instead of a <code>&lt;input
+    type="text"&gt;</code>`);
 
   let $ol = $('<ol>')
     .append($li1)

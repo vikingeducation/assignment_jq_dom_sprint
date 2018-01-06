ce967224d9b1ce81fcafd329e402a202b6cceb99
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-01-06 01:34:50 -0500

    finished h1 text change, first jquery code I've ever done!

---
+++ jq_warmup.js
@@ -0,0 +1,36 @@
+
+/*
+  cd Documents/Viking/JS/jq_warmup
+
+  if ( .length > 0) {
+    console.log("its working " + )
+  } else {
+    console.log("failure " + )
+  }
+
+*/
+
+$( document ).ready(function() {
+
+  $("h1").text("Something Cheeky");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});

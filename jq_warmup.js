f46daf31a5aa74aaf927a8c93ecd0b1605876c33
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-01-06 02:43:32 -0500

    finished 2 more jq changes

--- jq_warmup.js
+++ jq_warmup.js
@@ -1,5 +1,6 @@
 
 /*
+
   cd Documents/Viking/JS/jq_warmup
 
   if ( .length > 0) {
@@ -12,9 +13,31 @@
 
 $( document ).ready(function() {
 
-  $("h1").text("Something Cheeky");
+// Change the <h1> to something cheeky
+  $( "h1" ).text( "Something Cheeky" );
+
+/* Inside the container for "Upcoming Traversals", create and insert an
+ordered list of upcoming traversals. */
+
+  $( "info-box" );
 
 
+// Make all sad classes into happy ones.
+  $( ".sad" ).attr( "class", "happy");
+
+// Make the annoying popup link point instead to http://www.cashcats.biz.
+  $( "a[href='http://www.evilwebsite.com']" ).attr( "href", "http://www.cashcats.biz");
+
+/* Change the positioning of the annoying popup so it is on the right side of
+the screen (it's okay to use direct CSS here). Make it 30 pixels lower than
+its current position by utilizing its current top value. */
+  $( "#annoying-popup" ).css( { "top": "40px", "margin-left": "calc(100% - 231px)"} )
+
+/*Replace the ellipsis ... in one of the suggested topics with the content of
+your choice -- but do so by traversing from a different element. */
+
+
+/* Replace the form input with a <textarea> instead of a <input type="text">. */
 
 
 

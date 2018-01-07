8a5922ee3d62f6dd93dbe4d445d1f24899fcbbf1
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-01-07 07:06:47 -0500

    finished all but adding an ordered list

--- jq_warmup.js
+++ jq_warmup.js
@@ -1,59 +1,35 @@
 
-/*
-
-  cd Documents/Viking/JS/jq_warmup
-
-  if ( .length > 0) {
-    console.log("its working " + )
-  } else {
-    console.log("failure " + )
-  }
-
-*/
+// cd Documents/Viking/JS/jq_warmup
 
 $( document ).ready(function() {
 
 // Change the <h1> to something cheeky
-  $( "h1" ).text( "Something Cheeky" );
+  $( "h1" ).html( "Something Cheeky" );
 
 /* Inside the container for "Upcoming Traversals", create and insert an
 ordered list of upcoming traversals. */
 
-  $( "info-box" );
+  //$( "info-box" );
 
 
 // Make all sad classes into happy ones.
-  $( ".sad" ).attr( "class", "happy");
+  $( ".sad" ).attr( "class", "happy" );
 
 // Make the annoying popup link point instead to http://www.cashcats.biz.
-  $( "a[href='http://www.evilwebsite.com']" ).attr( "href", "http://www.cashcats.biz");
+  $( "a[href='http://www.evilwebsite.com']" ).attr( "href", "http://www.cashcats.biz" );
 
 /* Change the positioning of the annoying popup so it is on the right side of
 the screen (it's okay to use direct CSS here). Make it 30 pixels lower than
 its current position by utilizing its current top value. */
-  $( "#annoying-popup" ).css( { "top": "40px", "margin-left": "calc(100% - 231px)"} )
+  $( "#annoying-popup" ).css( { "top": "40px", "margin-left": "calc(100% - 231px)" } );
 
 /*Replace the ellipsis ... in one of the suggested topics with the content of
 your choice -- but do so by traversing from a different element. */
-
+  var $listItem = $( "ul li:nth-child(7)" );
+  $listItem.html( "Quality queries" );
 
 /* Replace the form input with a <textarea> instead of a <input type="text">. */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  var $test = $( "input" ).first();
+  $test.replaceWith( "<textarea></textarea>" )
 
 });
